1742c315bbc4df2bc43971e23d8442e93bff3941
sunkang <sky_sunkang@163.com>
2022-02-28 22:15:09 +0800

    init

---
+++ common/src/test/java/com/sunkang/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunkang.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ task/src/main/java/com/sunkang/task/TaskApplication.java
@@ -0,0 +1,15 @@
+package com.sunkang.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+
+}
