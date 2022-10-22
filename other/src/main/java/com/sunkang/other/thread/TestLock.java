56f9b85fc61d4637a408ff817ea4c0a3bf63c7d2
sunkang <sky_sunkang@163.com>
2022-10-22 22:24:02 +0800

    随便玩玩

--- other/src/main/java/com/sunkang/other/thread/TestLock.java
+++ other/src/main/java/com/sunkang/other/thread/TestLock.java
@@ -3,12 +3,13 @@ package com.sunkang.other.thread;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TestLock {
     public static void main(String[] args) throws InterruptedException {
         Pool pool = new Pool();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
             int finalI = i;
             new Thread(() -> {
                 pool.add(finalI);
@@ -16,9 +17,8 @@ public class TestLock {
             }).start();
         }
 
-        Thread.sleep(2000);
-        System.out.println(Thread.currentThread().getName() + " 1 " + pool.get().size());
-        System.out.println(Thread.currentThread().getName() + " 2 " + pool.getCow().size());
+        Thread.sleep(5000);
+        pool.printTotal();
     }
 }
 
@@ -26,26 +26,36 @@ class Pool {
     private static final ReentrantLock lock = new ReentrantLock();
     private static List<Integer> list = new ArrayList<>();
 
-    private static CopyOnWriteArrayList cowList=new CopyOnWriteArrayList();
+    private static Integer count = 0;
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    private static CopyOnWriteArrayList cowList = new CopyOnWriteArrayList();
 
     public void add(Integer str) {
         lock.lock();
-        try{
+        try {
             list.add(str);
-//            System.out.println(Thread.currentThread().getName() + " 1 " + list.size());
-        }finally {
+        } finally {
             lock.unlock();
         }
         cowList.add(str);
-//        System.out.println(Thread.currentThread().getName() + " 2 " + cowList.size());
+        count++;
+        atomicInteger.getAndIncrement();
     }
 
 
-    public List<Integer> get() {
-        return list;
-    }
-
-    public CopyOnWriteArrayList getCow(){
-        return cowList;
+    /**
+     * 打印并发结果
+     * 1:10000
+     * 2:10000
+     * 3:9998
+     * 4:10000
+     */
+    public void printTotal(){
+        System.out.println( "1:" + list.size());
+        System.out.println( "2:" + cowList.size());
+        System.out.println( "3:" + count);
+        System.out.println( "4:" + atomicInteger.get());
     }
 }
